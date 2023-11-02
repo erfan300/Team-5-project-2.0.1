e3e5074529fd6120f8c864ffc794f0ccc6524d44
H-Hafiz <160142172@aston.ac.uk>
2023-11-02 19:26:30 +0000

    Added section tag for book categories
    Added <section class="book-categories">, plus indentation

--- team-project/resources/views/Home.blade.php
+++ team-project/resources/views/Home.blade.php
@@ -30,20 +30,22 @@
     <p>Greetings and welcome to the X webpage, where I will be offering books in a variety of genres. We are the top sellers of specialized books that you won't find anywhere else.</p>
     
     <!-- Book Selection -->
-    <p><u>BEST SELLERS</u></p>
-    <img src="Image/BLANK.PNG" alt="Best Seller 1" title="Best Seller 1">
-    <img src="Image/BLANK.PNG" alt="Best Seller 2" title="Best Seller 2">
-    <img src="Image/BLANK.PNG" alt="Best Seller 3" title="Best Seller 3">
-    <img src="Image/BLANK.PNG" alt="Best Seller 4" title="Best Seller 4">
-    <p><u>NEW IN</u></p>
-    <img src="Image/BLANK.PNG" alt="New In 1" title="New In 1">
-    <img src="Image/BLANK.PNG" alt="New In 2" title="New In 2">
-    <img src="Image/BLANK.PNG" alt="New In 3" title="New In 3">
-    <img src="Image/BLANK.PNG" alt="New In 4" title="New In 4">
-    <p><u>GENRES</u></p>
-    <img src="Image/BLANK.PNG" alt="Genre 1" title="Genre 1">
-    <img src="Image/BLANK.PNG" alt="Genre 2" title="Genre 2">
-    <img src="Image/BLANK.PNG" alt="Genre 3" title="Genre 3">
-    <img src="Image/BLANK.PNG" alt="Genre 4" title="Genre 4">
+    <section class="book-categories">
+        <p><u>BEST SELLERS</u></p>
+            <img src="Image/BLANK.PNG" alt="Best Seller 1" title="Best Seller 1">
+            <img src="Image/BLANK.PNG" alt="Best Seller 2" title="Best Seller 2">
+            <img src="Image/BLANK.PNG" alt="Best Seller 3" title="Best Seller 3">
+            <img src="Image/BLANK.PNG" alt="Best Seller 4" title="Best Seller 4">
+        <p><u>NEW IN</u></p>
+            <img src="Image/BLANK.PNG" alt="New In 1" title="New In 1">
+            <img src="Image/BLANK.PNG" alt="New In 2" title="New In 2">
+            <img src="Image/BLANK.PNG" alt="New In 3" title="New In 3">
+            <img src="Image/BLANK.PNG" alt="New In 4" title="New In 4">
+        <p><u>GENRES</u></p>
+            <img src="Image/BLANK.PNG" alt="Genre 1" title="Genre 1">
+            <img src="Image/BLANK.PNG" alt="Genre 2" title="Genre 2">
+            <img src="Image/BLANK.PNG" alt="Genre 3" title="Genre 3">
+            <img src="Image/BLANK.PNG" alt="Genre 4" title="Genre 4">
+    </section>
 </body>
 </html>
