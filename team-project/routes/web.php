c0400cd068b578a1153f9119397049c1421574f7
Erfan Soltani <220117609@aston.ac.uk>
2023-11-02 17:19:30 +0000

    Fixed Routing Issue the Back-End devs had
    We were unsure of why the URL was going to a Laravel Documentation Page but I fixed the issue since it was a Routing problem

--- team-project/routes/web.php
+++ team-project/routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/homepage', function () {
+Route::get('/home', function () {
     return view('Home');
 });
