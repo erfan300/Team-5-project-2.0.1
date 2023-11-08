27395c3a37ca1f2cdbf623c69c35600afbd24594
TamaraFufeyin <220068149@aston.ac.uk>
2023-11-08 23:55:22 +0000

    Updated nav bar to link to page basket

--- team-project/resources/views/Basket.blade.php
+++ team-project/resources/views/Basket.blade.php
@@ -24,10 +24,10 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link" href="">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Basket</a>
+                    <a class="nav-link" href="Basket.blade.html">Basket</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">About us</a>
--- team-project/resources/views/Signup.blade.php
+++ team-project/resources/views/Signup.blade.php
@@ -19,26 +19,26 @@
 <main>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Books4U</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Basket</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">About us</a>
+        <a class="navbar-brand" href="#">Books4U</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="">Home</a>
+                </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Log In/Sign In </a>
-            </li>
-        </ul>
-    </div>
-</nav> 
+                    <a class="nav-link" href="Basket.blade.html">Basket</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About us</a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Log In/Sign In </a>
+                </li>
+            </ul>
+        </div>
+    </nav> 
 
 <h1>Sign up</h1>
 @if ($errors->any())
