862540162d6491a4fca80ba61ea73070a5013f0c
TamaraFufeyin <220068149@aston.ac.uk>
2023-11-09 13:08:18 +0000

    added navBar

--- team-project/resources/views/login.blade.php
+++ team-project/resources/views/login.blade.php
@@ -8,21 +8,37 @@
     <title>Books4U</title>
     <link href="{{ asset('css/logincss.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <link rel="icon" href="" type="" />
+    <link rel="stylesheet" type="text/css" href="/" />
+    <link rel="stylesheet" type="text/css" href="css/style.css"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"/>
+    <script defer src="js/main.js"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   </head>
   
     <!--Navigation start-->
-    <header class="header">
-        
-        <a href="#"><img src="images/logo.png" class="logo" alt="logo"></a>
-        
-        <nav class="navbar">
-            <a href="#">Home</a>
-            <a href="#">Products</a>
-            <a href="#">About us</a>
-            <a href="#">Sign up</a>
-            <a href="#">Log in</a>
-        </nav>
-    </header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Books4U</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="Home.blade.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Basket.blade.html">Basket</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.blade.html">About us</a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.blade.html">Log In/Sign In </a>
+                </li>
+            </ul>
+        </div>
+    </nav> 
 
     <!--Navigation end-->
 
