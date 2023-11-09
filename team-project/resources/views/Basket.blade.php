db31164e1f2a83e1f517d6973c0c8fd6dea1f2fe
TamaraFufeyin <220068149@aston.ac.uk>
2023-11-09 11:39:16 +0000

    Added a continue to checkout button

--- team-project/resources/views/Basket.blade.php
+++ team-project/resources/views/Basket.blade.php
@@ -63,8 +63,13 @@
             </tfoot>
         </table>
 
-        <button class="checkout">Checkout</button>
+        <form>
+       
+    
+            <input type="submit" value="Continue to checkout" class="btn">
+          </form>
     </section>
+     
  
     <!-- Bootstrap & jQuery JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
