dc3195ddfcca3e139821c86f17df1cff104e4287
H-Hafiz <160142172@aston.ac.uk>
2023-11-14 22:52:52 +0000

    Fixed Homepage layout
    Moved css to seperate css file (homeStyle.css). Moved javascript to javascript file (custom.js). Fixed slideshow and added 3 placeholder images. Changed it so that the slides changed everything 6 seconds. Fixed homepage layout.

--- team-project/public/js/custom.js
+++ team-project/public/js/custom.js
@@ -12,3 +12,22 @@ document.addEventListener('DOMContentLoaded', function() {
         alertElement.addEventListener('animationend', handleAnimationEnd);
     }, 2000); 
 });
+
+//Function for Slide Show 
+let slideIndex = 0;
+
+        function showSlides() {
+            let i;
+            const slides = document.getElementsByClassName("mySlides");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) { slideIndex = 1 }
+            slides[slideIndex - 1].style.display = "block";
+            setTimeout(showSlides, 6000); // Change slide every 6 seconds
+        }
+
+        document.addEventListener("DOMContentLoaded", function () {
+            showSlides();
+        });
--- team-project/resources/views/index.blade.php
+++ team-project/resources/views/index.blade.php
@@ -3,144 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <title>Books4U Bookstore</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <style>
-        body {
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f8f9fa;
-            color: #495057;
-        }
-
-        header {
-            background-color: #343a40;
-            color: #fff;
-            padding: 20px;
-            text-align: center;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-        }
-
-        nav {
-            background-color: #495057;
-            padding: 10px;
-            text-align: center;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        }
-
-        nav a {
-            color: #fff;
-            text-decoration: none;
-            padding: 10px;
-            margin: 0 15px;
-            font-weight: bold;
-            transition: color 0.3s ease;
-        }
-
-        nav a:hover {
-            color: #17a2b8;
-        }
-
-        .search-form {
-            text-align: center;
-            margin-top: 20px;
-        }
-
-        .search-container {
-            display: flex;
-        }
-
-        .search-input {
-            flex: 1;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
-
-        .search-button-container {
-            margin-left: 10px;
-        }
-
-        .book-categories {
-            margin-top: 40px;
-        }
-
-        .book-category {
-            margin-bottom: 40px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            border-radius: 5px;
-            overflow: hidden;
-        }
-
-        .book-category h3 {
-            background-color: #343a40;
-            color: #fff;
-            padding: 10px;
-            margin: 0;
-        }
-
-        .book-genre-container {
-            padding: 20px;
-        }
-
-        .book-genre-container p {
-            margin: 0;
-        }
-
-        .footer {
-            text-align: center;
-            margin-top: 40px;
-            background-color: #343a40;
-            color: #fff;
-            padding: 20px;
-        }
-
-        .nav-link-cart {
-            display: flex;
-            align-items: center;
-            color: #17a2b8;
-            text-decoration: none;
-            transition: color 0.3s ease;
-        }
-
-        .nav-link-cart i {
-            margin-right: 5px;
-        }
-
-        .slideshow-container {
-            max-width: 1000px;
-            margin: auto;
-            margin-bottom: 40px;
-            overflow: hidden;
-            border-radius: 5px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        }
-
-        .mySlides {
-            display: none;
-            animation: fade 4s ease-in-out infinite;
-        }
-
-        @keyframes fade {
-            from {
-                opacity: 0.4;
-            }
-            to {
-                opacity: 1;
-            }
-        }
-
-        img {
-            width: 100%;
-            height: auto;
-            border-radius: 5px;
-        }
-    </style>
+    <link href="{{ asset('css/homeStyle.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/custom.js') }}"></script>
 </head>
 <body>
+    <!-- Display login flash Message -->
+    @if (session('message'))
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
+    @endif
     <header>
         <h1>Books4U Bookstore</h1>
     </header>
@@ -167,29 +42,6 @@
                         <i class="fas fa-shopping-cart"></i> Cart
                     </a>
                 </li>
-=======
-    <link href="{{ asset('css/homeStyle.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/custom.js') }}"></script>
-    <title>Homepage</title>
-</head>
-<body>
-    <!-- Display login flash Message -->
-    @if (session('message'))
-        <div class="alert alert-success">
-            {{ session('message') }}
-        </div>
-    @endif
-    
-    <!-- Header -->
-    <section class="header">
-        <nav>
-            <ul>
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-                <li><a href="#">Link 4</a></li>
-                <li><a href="#">Link 5</a></li>
->>>>>>> b1951e1cb54d1cf4d779368e9be0ddda482533a7
             </ul>
         </div>
     </nav>
@@ -199,7 +51,7 @@
         <div class="search-container">
             <input type="text" name="search" class="search-input" placeholder="Search Books4U..." />
             <div class="search-button-container">
-                <button type="submit" class="btn btn-primary">Search</button>
+                <button type="submit" class="search-button">Search</button>
             </div>
         </div>
     </form>
@@ -207,39 +59,20 @@
     <!-- Slideshow -->
     <div class="slideshow-container">
         <div class="mySlides">
-            <img src="C:/Users/Raihan/Documents/GitHub/eam-5-project-2.0.1/team-project/public/images/new/slide1.jpg" alt="Slide 1">
+            <img src="{{ asset('images/slide_placeholder_1.png') }}" alt="Slide 1">
         </div>
         <div class="mySlides">
-            <img src="path/to/slide2.jpg" alt="Slide 2">
+            <img src="{{ asset('images/slide_placeholder_2.png') }}" alt="Slide 2">
         </div>
         <div class="mySlides">
-            <img src="path/to/slide3.jpg" alt="Slide 3">
+            <img src="{{ asset('images/slide_placeholder_3.jpg') }}" alt="Slide 3">
         </div>
     </div>
-    <script>
-        let slideIndex = 0;
-
-        function showSlides() {
-            let i;
-            const slides = document.getElementsByClassName("mySlides");
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slideIndex++;
-            if (slideIndex > slides.length) { slideIndex = 1 }
-            slides[slideIndex - 1].style.display = "block";
-            setTimeout(showSlides, 4000); // Change slide every 4 seconds
-        }
-
-        document.addEventListener("DOMContentLoaded", function () {
-            showSlides();
-        });
-    </script>
 
     <!-- Book Selection -->
     <section class="book-categories">
+        <h3>BEST SELLERS</h3>
         <div class="book-category">
-            <h3>BEST SELLERS</h3>
             <div class="book-genre-container">
                 @if(count($bestSellers) == 0)
                     <p>No Books found</p>
@@ -249,8 +82,8 @@
                 @endforeach
             </div>
         </div>
+        <h3>Fiction</h3>
         <div class="book-category">
-            <h3>Fiction</h3>
             <div class="book-genre-container">
                 @if(count($fiction) == 0)
                     <p>No Books found</p>
@@ -260,13 +93,6 @@
                 @endforeach
             </div>
         </div>    
-        <h3>GENRES</h3>
-        <div class="genre-container">
-            <img src="Image/BLANK.PNG" alt="Genre 1" title="Genre 1">
-            <img src="Image/BLANK.PNG" alt="Genre 2" title="Genre 2">
-            <img src="Image/BLANK.PNG" alt="Genre 3" title="Genre 3">
-            <img src="Image/BLANK.PNG" alt="Genre 4" title="Genre 4">
-        </div>
     </section>
     
     <!-- Footer -->
