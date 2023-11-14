4246e78cc991d828d7ca01a2309aad6bcfdd85f9
jeevangheer <jeevangheer31@gmail.com>
2023-11-14 12:25:29 +0000

    updated about/contact page
    updated the about/contact page

--- team-project/resources/views/about.blade.php
+++ team-project/resources/views/about.blade.php
@@ -6,65 +6,112 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Books4U</title>
-    <link href="{{ asset('css/aboutcss.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
-  </head>
-
-  <!--Navigation start-->
-  <header class="header">
-        
-    <a href="#"><img src="images/logo.png" class="logo" alt="logo"></a>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="css/aboutcss.css">
+    </head>
+<body>
     
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo">
+        <img src="images/logo.png" alt="">
+    </a>
+
     <nav class="navbar">
         <a href="#">Home</a>
+        <a href="#about">About</a>
         <a href="#">Products</a>
-        <a href="#">About us</a>
-        <a href="#">Sign up</a>
-        <a href="#">Log in</a>
+        <a href="#contact">Contact</a>
+        <a href="#">Sign up/log in</a>
     </nav>
 </header>
 
-<!--Navigation end-->
+<!-- header section ends -->
 
-<!--About us section start-->
 <br><br><br><br><br><br><br><br>
-    <body>
-        <div class="heading">
-            <h1>About Us</h1>
-            </div>
-            <div class="container">
-                <section class="about">
-                    <div class="about-content">
-                        <h2>About us information.......</h2>
-                    </div>
-                </section>
-            </div>
-        </div>
-        
-        
-        <br><br><br><br><br><br>
 
-         <!--About section end-->
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <h1 class="heading"> <span>about</span> us </h1>
+
+    <div class="row">
+
+        <div class="image">
+       
+        </div>
+
+        <div class="content">
+            <h3>Books4U</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus qui ea ullam, enim tempora ipsum fuga alias quae ratione a officiis id temporibus autem? Quod nemo facilis cupiditate. Ex, vel?</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit amet enim quod veritatis, nihil voluptas culpa! Neque consectetur obcaecati sapiente?</p>
+            <a href="#contact" class="btn">Contact us</a>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+    <h1 class="heading"> <span>contact</span> us </h1>
+
+</section>
+
+<!-- contact section ends -->
 
          <!--contact section start-->
-    <div class="contact-section">
-        <section class="contact-info">
-          <div><i class="fas fa-map-marker-alt"></i>Aston University, Birmingham</div>
-          <div><i class="fas fa-envelope"></i>info@books4u.com</div>
-          <div><i class="fas fa-phone"></i>0121 204 3000</div>
-        </section>
+         <div class="contact-section">
+            <section class="contact-info">
+              <div><i class="fas fa-map-marker-alt"></i>Aston University, Birmingham</div>
+              <div><i class="fas fa-envelope"></i>info@books4u.com</div>
+              <div><i class="fas fa-phone"></i>0121 204 3000</div>
+            </section>
+    
+    
+            <section class="contact-form">
+              <h2>Books4U</h2>
+              <form class="contact" action="" method="post">
+                <input type="text" name="name" class="text-box" placeholder="Your Name" required>
+                <input type="email" name="email" class="text-box" placeholder="Your Email" required>
+                <textarea name="message" rows="5" placeholder="Your message" required></textarea>
+                <input type="submit" name="submit" class="send-btn" value="Send">
+              </form>
+            </section>
+          </div>
+          <!--contact section end-->
 
-        <section class="contact-form">
-          <h2>Contact Us</h2>
-          <form class="contact" action="" method="post">
-            <input type="text" name="name" class="text-box" placeholder="Your Name" required>
-            <input type="email" name="email" class="text-box" placeholder="Your Email" required>
-            <textarea name="message" rows="5" placeholder="Your message" required></textarea>
-            <input type="submit" name="submit" class="send-btn" value="Send">
-          </form>
-        </section>
-      </div>
-      <!--contact section end-->
 
-  </body>
+          <!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="share">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        <a href="#" class="fab fa-pinterest"></a>
+    </div>
+
+    <div class="links">
+        <a href="#">Home</a>
+        <a href="#about">About</a>
+        <a href="#">Products</a>
+        <a href="#contact">Contact</a>
+        <a href="#">Sign up/log in</a>
+    </div>
+
+</section>
+
+<!-- footer section ends -->
+</body>
 </html>
