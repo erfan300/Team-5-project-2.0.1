a4f165f0888ed142d7ce7504d8b565afde9192ab
TamaraFufeyin <220068149@aston.ac.uk>
2023-11-21 11:25:16 +0000

    added bootstrap

--- team-project/resources/views/Signup.blade.php
+++ team-project/resources/views/Signup.blade.php
@@ -107,5 +107,9 @@
 
 </footer>
 
+ <!-- Bootstrap & jQuery JS -->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="script.js"></script>
 </body>
 </html>
