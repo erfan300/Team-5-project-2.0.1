ad190e5595dfce315bb47510978008d3a8bbb64b
TamaraFufeyin <220068149@aston.ac.uk>
2023-11-28 10:41:56 +0000

    basket page redesign

--- team-project/resources/views/Basket.blade.php
+++ team-project/resources/views/Basket.blade.php
@@ -4,74 +4,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Books4U</title>
-   
+    <!DOCTYPE html>
+<html lang="en">
+<head>
     <link rel="icon" href="" type="" />
     <link rel="stylesheet" type="text/css" href="/" />
-    <link rel="stylesheet" type="text/css" href="css/style.css"
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="{{ asset('css/basket.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <script defer src="js/main.js"></script>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
 </head>
 
 <body>
+<header>
+        <h1>Books4U Bookstore</h1>
+    </header>
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Books4U</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="Home.blade.html">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="Basket.blade.html">Basket</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="about.blade.html">About us</a>
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.blade.html">Log In/Sign In </a>
-                </li>
-            </ul>
-        </div>
-    </nav> 
+    <nav>
+    <a href="home">Home</a>
+    <a href="profile">Profile</a>
+    <a href="basket">Basket</a>
+    <a href="login">Log In</a>
+    <a href="about">About</a>
+    <a href="contact">Contact</a>
+</nav>
  
-    <!-- Shopping Basket -->
-    <section id="shopping-basket" class="container mt-5">
-        <h2>Shopping Basket</h2>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Product</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Total</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody id="basket-items">
-                <!-- basket items will be dynamically added here -->
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td colspan="3"></td>
-                    <td><strong>Total:</strong></td>
-                    <td id="basket-total">£0.00</td>
-                </tr>
-            </tfoot>
-        </table>
+    <!-- Shopping Basket Items -->
+    <div class="small-container basket-page">
+        <table>
+            <tr>
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+</tr>
 
-        <form action="Paymentpage.html">
-            <input type="submit" value="Continue to checkout" class="btn">
-        </form>
-    </section>
-     
+<div class="total-price">
+
+<table>
+    <tr>
+        <td>Total</td>
+        <td>£0</td>
+</tr>
+
+</table>
+
+
  
-    <!-- Bootstrap & jQuery JS -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     <script src="script.js"></script>
 </body>
 </html>
