a375cb750acdb389a7b0b0c4c06074a50f664a79
jeevangheer <jeevangheer31@gmail.com>
2023-11-28 01:12:35 +0000

    updated css for login page

--- team-project/resources/views/login.blade.php
+++ team-project/resources/views/login.blade.php
@@ -16,19 +16,6 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   </head>
-  
-    <!--Navigation start-->
-    <nav>
-    <a href="home">Home</a>
-    <a href="profile">Profile</a>
-    <a href="basket">Basket</a>
-    <a href="login">Log In</a>
-    <a href="about">About</a>
-    <a href="contact">Contact</a>
-</nav>
-
-    <!--Navigation end-->
-
 
     <!--Login form start-->
     @if (count($errors) > 0)
@@ -40,22 +27,38 @@
         </ul>
     </div>
 @endif
+
     <body>
+    <header class="header">
+
+    <a href="#" class="logo">
+        <img src="images/logo.png" alt="">
+    </a>
+
+    <nav class="navbar">
+    <a href="home">Home</a>
+    <a href="profile">Profile</a>
+    <a href="basket">Basket</a>
+    <a href="login">Log In</a>
+    <a href="about">About</a>
+    <a href="contact">Contact</a>
+    </nav>
+
+</header>
+
+
+
         <div class="wrapper">
             <div class="form-box login">
                 <h2>Login</h2>
                 <form method="POST" action="/authenticate">
                     @csrf
                     <div class="input-box">
-                        <span class="icon">
-                            <ion-icon name="mail"></ion-icon>
-                            </span>
                             <input type="email" name="email" required>
                             <label>Email</label>
                         </div>
         
                         <div class="input-box">
-                            <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
                             <input type="password" name="password" required>
                             <label>Password</label>
                         </div>
@@ -70,4 +73,182 @@
         </div>
   </body>
   <!--Login form end-->
+
+
+  <style>
+    
+    :root{
+        --blue:#6c96b6;
+        --darkgreen:#384b42;
+        --lightgreen:#6a9b86;
+        --border:.1rem solid #6a9b86;
+    }
+    
+    *{
+        font-family: 'Roboto', sans-serif;
+        margin:0; padding:0;
+        box-sizing: border-box;
+        outline: none; border:none;
+        text-decoration: none;
+        text-transform: capitalize;
+        transition: .2s linear;
+    }
+    
+    html{
+        font-size: 62.5%;
+        scroll-behavior: smooth;
+    }
+    
+    
+    
+    body{
+        background: var(--darkgreen);
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    
+
+    
+    .header{
+        background: var(--darkgreen);
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding:1.5rem 7%;
+        border-bottom: var(--border);
+        position: fixed;
+        top:0; left: 0; right: 0;
+        
+    }
+    
+    .logo img{
+        height: 7rem;
+    }
+    
+    .header .navbar a{
+       
+        font-size: 15px;
+        color:#fff;
+    }
+    
+    .header .navbar a:hover{
+        color:var(--blue);
+        border-bottom: .1rem solid var(--blue);
+        padding-bottom: .5rem;
+    }
+    
+    
+    
+    
+    /* NAVIGATION CSS */
+    
+    .navbar a{
+        position: relative;
+        font-size: 18px;
+        color: #fff;
+        font-weight: 500;
+        text-decoration: none;
+        margin-left: 40px;
+    }
+    
+    
+    
+       /* LOGIN CSS */
+    
+       .wrapper{
+        width: 500px;
+        height: 500px;
+        background: var(--lightgreen);
+        border-radius: 25px;
+        display: flex;
+        align-items: center;
+      }
+      
+      .form-box{
+        width: 100%;
+        padding: 50px;
+      }
+    
+      .form-box h2{
+        font-size: 20px;
+        font-weight: bold;
+        color: black;
+        text-align: center;
+      }
+    
+      .input-box{
+        width: 100%;
+        height: 65px;
+        position: relative;
+        border-bottom: 3px solid black;
+        margin: 40px 0;
+      }
+      
+      
+    
+    
+      .input-box label {
+        position: absolute;
+        top: 50%;
+        margin-left: 5px;
+        font-size: 11px;
+        color: black;
+        font-weight: bold;
+        
+      }
+    
+      .input-box input:focus~label,
+      .input-box input:valid~label {
+        top: -10px;
+      }
+    
+      .input-box input{
+        height: 100%;
+        width: 100%;
+        font-size: 10px;
+        color: black;
+        background: transparent;
+        font-weight: bold;
+        margin-left: 5px;
+      }
+    
+      
+    
+      .login-register {
+        font-size: 10px;
+        color: black;
+        text-align: center;
+        margin: 2em;
+      }
+    
+      .login-register p a {
+        color: black;
+        font-weight: bold;
+      }
+    
+      .login-register p a:hover {
+        text-decoration: underline;
+        color: var(--blue);
+      }
+    
+    
+      .btn{
+        background: var(--darkgreen);
+        height: 50px;
+        width: 100%;
+        border-radius: 7px;
+        cursor: pointer;
+        font-size: 10px;
+        color: #fff;
+        font-weight: bold;
+      }
+      
+      .btn:hover{
+        color: var(--blue);
+    }
+    
+    </style>
+
 </html>
