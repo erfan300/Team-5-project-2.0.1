f7a73df21d86e0187c3200be4cae0541bc1b48fe
MrGul1 <210059645@aston.ac.uk>
2023-11-29 14:54:45 +0000

    bootstrap removed from basket, login, password, register
    removed bootstrap from the listed pages some css has changed but will work on ensuring the look and feel off the website remains the same

--- team-project/resources/views/Basket.blade.php
+++ team-project/resources/views/Basket.blade.php
@@ -1,28 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Books4U</title>
     <link rel="icon" href="" type="" />
     <link rel="stylesheet" type="text/css" href="/" />
-    <link rel="stylesheet" type="text/css" href="css/style.css"
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"/>
     <script defer src="js/main.js"></script>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
 <body>
     <!-- Navigation -->
     <nav>
-    <a href="home">Home</a>
-    <a href="profile">Profile</a>
-    <a href="basket">Basket</a>
-    <a href="login">Log In</a>
-    <a href="about">About</a>
-    <a href="contact">Contact</a>
-</nav>
+        <a href="home">Home</a>
+        <a href="profile">Profile</a>
+        <a href="basket">Basket</a>
+        <a href="login">Log In</a>
+        <a href="about">About</a>
+        <a href="contact">Contact</a>
+    </nav>
 
     <!-- Shopping Basket -->
     <section id="shopping-basket" class="container mt-5">
@@ -53,87 +50,82 @@
             <input type="submit" value="Continue to checkout" class="btn">
         </form>
     </section>
-     
- 
-    <!-- Bootstrap & jQuery JS -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="script.js"></script>
-</body>
-</html>
 
-<style>
-:root {
-    --blue: #6c96b6;
-    --darkgreen: #6a9b86;
-    --lightgreen: #c8e6d1;
-    --border: .1rem solid #6a9b86;
-}
+    <style>
+        :root {
+            --blue: #6c96b6;
+            --darkgreen: #6a9b86;
+            --lightgreen: #c8e6d1;
+            --border: .1rem solid #6a9b86;
+        }
 
-body {
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    margin: 0;
-    padding: 0;
-    background-color: var(--darkgreen);
-    color: var(--lightgreen);
-}
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: var(--darkgreen);
+            color: var(--lightgreen);
+        }
 
-nav {
-    background-color: var(--darkgreen);
-    overflow: hidden;
-    text-align: center;
-}
+        nav {
+            background-color: var(--darkgreen);
+            overflow: hidden;
+            text-align: center;
+        }
 
-nav a {
-    display: inline-block;
-    color: var(--lightgreen);
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    transition: background-color 0.3s;
-}
+        nav a {
+            display: inline-block;
+            color: var(--lightgreen);
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            transition: background-color 0.3s;
+        }
 
-nav a:hover {
-    background-color: var(--blue);
-    color: black;
-}
+        nav a:hover {
+            background-color: var(--blue);
+            color: black;
+        }
 
-/* Shopping Basket Section */
+        /* Shopping Basket Section */
 
-#shopping-basket {
-    background-color: var(--lightgreen);
-    color: var(--darkgreen); /* Text color set to darkgreen */
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-}
+        #shopping-basket {
+            background-color: var(--lightgreen);
+            color: var(--darkgreen);
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        }
 
-#shopping-basket h2 {
-    color: var(--darkgreen);
-}
+        #shopping-basket h2 {
+            color: var(--darkgreen);
+        }
 
-.table th,
-.table td {
-    color: var(--darkgreen);
-}
+        .table th,
+        .table td {
+            color: var(--darkgreen);
+        }
 
-.table tfoot td {
-    color: var(--darkgreen);
-    font-weight: bold;
-}
+        .table tfoot td {
+            color: var(--darkgreen);
+            font-weight: bold;
+        }
 
-.btn {
-    background: var(--darkgreen);
-    color: #fff;
-    border: none;
-    padding: 10px 20px;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: 16px;
-    transition: background 0.2s linear;
-}
+        .btn {
+            background: var(--darkgreen);
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 16px;
+            transition: background 0.2s linear;
+        }
 
-.btn:hover {
-    background: var(--blue);
-}
+        .btn:hover {
+            background: var(--blue);
+        }
     </style>
+</body>
+
+</html>
--- team-project/resources/views/password.blade.php
+++ team-project/resources/views/password.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,50 +9,41 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <link rel="icon" href="" type="" />
     <link rel="stylesheet" type="text/css" href="/" />
-    <link rel="stylesheet" type="text/css" href="css/style.css"
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
     <script defer src="js/main.js"></script>
+</head>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  </head>
-
-    <!--Navigation start-->
-    <body>
+<body>
     <header class="header">
+        <a href="#" class="logo">
+            <img src="images/logo.png" alt="">
+        </a>
+        <nav class="navbar">
+            <a href="home">Home</a>
+            <a href="profile">Profile</a>
+            <a href="basket">Basket</a>
+            <a href="login">Log In</a>
+            <a href="about">About</a>
+            <a href="contact">Contact</a>
+        </nav>
+    </header>
 
-    <a href="#" class="logo">
-        <img src="images/logo.png" alt="">
-    </a>
-
-    <nav class="navbar">
-    <a href="home">Home</a>
-    <a href="profile">Profile</a>
-    <a href="basket">Basket</a>
-    <a href="login">Log In</a>
-    <a href="about">About</a>
-    <a href="contact">Contact</a>
-    </nav>
-
-</header>
-
-    <!--Navigation end-->
-
-
-       <!--Change Password form start-->
-       @if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
+    <body>
+        @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
-    <div class="wrapper">
-        <div class="form-box change-password">
-            <h2>Change Password</h2>
-            <form method="POST" action="/change-password">
-                @csrf
+                @endforeach
+            </ul>
+        </div>
+        @endif
+        <div class="wrapper">
+            <div class="form-box change-password">
+                <h2>Change Password</h2>
+                <form method="POST" action="/change-password">
+                    @csrf
+
                 <div class="input-box">
                     <span class="icon">
                         <ion-icon name="mail"></ion-icon>
