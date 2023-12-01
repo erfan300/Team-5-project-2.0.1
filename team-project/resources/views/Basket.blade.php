b3295f9918efca7a6e9eb5952a0f644fed93d1ba
TamaraFufeyin <220068149@aston.ac.uk>
2023-12-01 00:00:51 +0000

    styled basket page

--- team-project/resources/views/Basket.blade.php
+++ team-project/resources/views/Basket.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Books4U</title>
    <link rel="icon" href="" type="" />
-    <link rel="stylesheet" type="text/css" href="/" />
+    <link rel="stylesheet" type="text/css" href="ccs/basket.css" />
     <script defer src="js/main.js"></script>
 
 </head>
@@ -24,11 +24,14 @@
         <a href="about">About</a>
         <a href="contact">Contact</a>
     </nav>
-                
- 
+        <br><br>    
+
     <!-- Shopping Basket -->
+
     <section id="shopping-basket" class="container mt-5">
-        <h2>Shopping Basket</h2>
+<header>
+    <h2>Shopping Basket</h2>
+    <header>
         <table class="table">
             <thead>
             <tr>
@@ -43,10 +46,7 @@
                 <h3>Quantity<h3>
 
                 </th>
-            <th>
-                <h3>Subtotal<h3>
 
-                <th></th>
 
 
 </tr>
@@ -77,6 +77,110 @@
         </form>
     </section>
      
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap');
+
+:root{
+    --blue:#6c96b6;
+    --darkgreen:#384b42;
+    --lightgreen:#6a9b86;
+    --border:.1rem solid #6a9b86;
+}
+
+body {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: var(--lightgreen);
+    color: var(--lightgreen);
+}
+
+
+/* Header Styling */
+header {
+    background-color: var(--lightgreen);
+    color:#a7eecf ;
+    padding: 20px;
+    text-align: center;
+}
+
+
+/* Navigation Styling */
+
+nav {
+            background-color: var(--darkgreen);
+            overflow: hidden;
+            text-align: center;
+        }
+
+        nav a {
+            display: inline-block;
+            color: var(--lightgreen);
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            transition: background-color 0.3s;
+        }
+
+        nav a:hover {
+            background-color: var(--blue);
+            color: black;
+        }
+
+
+/*Shopping Basket Items*/
+
+.basket-page{
+    margin: 80px auto;
+}
+table{
+    width:95%;
+    border:collapse;
+    margin:auto;
+    background-color:white;
+    height:100px;         
+}
+
+h2{
+    position: absolute;
+  left: 100px;
+  top: 200px;
+}
+
+
+.basket-page thead tr{
+    background-color:black;
+    font-weight:normal ;
+    text-align: left;
+    padding: 60px;
+
+}
+.btn {
+    position: absolute;
+    right: 130px;
+    top:480px;
+            background: var(--darkgreen);
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 16px;
+            transition: background 0.2s linear;
+        }
+
+        .btn:hover {
+            background: var(--blue);
+        }
+
+
+</style>
+
+
+
+
+
+
 </body>
 
 </html>
