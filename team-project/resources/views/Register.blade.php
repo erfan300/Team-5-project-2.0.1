05860fb90936971067c52949fb81d9bd5e1e6d62
MrGul1 <210059645@aston.ac.uk>
2023-12-04 17:02:32 +0000

    css html update
    added some css and html to sign up not much still working on it

--- team-project/resources/views/Register.blade.php
+++ team-project/resources/views/Register.blade.php
@@ -4,9 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Books4U</title>
+    <title>Books4U Sign Up</title>
     <link rel="icon" href="" type="" />
-    <link rel="stylesheet" type="text/css" href="/" />
     <script defer src="js/main.js"></script>
 </head>
 
@@ -23,66 +22,75 @@
             <a href="contact">Contact</a>
         </nav>
 
-        <h1>Sign up</h1>
-        @if ($errors->any())
+        <form method="POST" action="/users">
+            <h1>Sign up</h1>
+            @if ($errors->any())
             <div class="alert">
                 <ul>
                     @foreach ($errors->all() as $error)
-                        <p>{{ $error }}</p>
+                    <p>{{ $error }}</p>
                     @endforeach
                 </ul>
             </div>
-        @endif
-        <form method="POST" action="/users">
+            @endif
             @csrf
             <input type="text" name="first_name" placeholder="First Name" required><br><br>
             @error('first_name')
-                <p class="first-name-error">{{$message}}</p>
-            @enderror
-            <input type="text" name="last_name" placeholder="Last Name" required><br><br>
-            @error('last_name')
-                <p class="last-name-error">{{$message}}</p>
-            @enderror
-            <input type="text" name="username" placeholder="Username" required><br><br>
-            @error('username')
-                <p class="username-error">{{$message}}</p>
-            @enderror
-            <input type="email" name="email" placeholder="Email Address" required><br><br>
-            @error('email')
-                <p class="email-error">{{$message}}</p>
-            @enderror
-            <input type="email" name="email_confirmation" placeholder="Confirm Email Address" required><br><br>
-            @error('email_confirmation')
-                <p class="confirm-email-error">{{$message}}</p>
-            @enderror
-            <input type="password" name="password" placeholder="Password" required><br><br>
-            @error('password')
-                <p class="password-error">{{$message}}</p>
-            @enderror
-            <input type="password" name="password_confirmation" placeholder="Confirm Password" required><br><br>
-            @error('password_confirmation')
-                <p class="confirm-password-error">{{$message}}</p>
-            @enderror
-            <input type="text" name="address" placeholder="Address"><br><br>
-            @error('address')
-                <p class="address-error">{{$message}}</p>
-            @enderror
-            <input type="text" name="phone_number" placeholder="Phone Number"><br><br>
-            @error('phone_number')
-                <p class="phone-number-error">{{$message}}</p>
+            <p class="error-message">{{$message}}</p>
             @enderror
 
-            <label for="user_type">Select User Type:</label>
+            <input type="text" name="last_name" placeholder="Last Name" required><br><br>
+            @error('last_name')
+            <p class="error-message">{{$message}}</p>
+            @enderror
+
+            <input type="text" name="username" placeholder="Username" required><br><br>
+            @error('username')
+            <p class="error-message">{{$message}}</p>
+            @enderror
+
+            <input type="email" name="email" placeholder="Email Address" required><br><br>
+            @error('email')
+            <p class="error-message">{{$message}}</p>
+            @enderror
+
+            <input type="email" name="email_confirmation" placeholder="Confirm Email Address" required><br><br>
+            @error('email_confirmation')
+            <p class="error-message">{{$message}}</p>
+            @enderror
+
+            <input type="password" name="password" placeholder="Password" required><br><br>
+            @error('password')
+            <p class="error-message">{{$message}}</p>
+            @enderror
+
+            <input type="password" name="password_confirmation" placeholder="Confirm Password" required><br><br>
+            @error('password_confirmation')
+            <p class="error-message">{{$message}}</p>
+            @enderror
+
+            <input type="text" name="address" placeholder="Address"><br><br>
+            @error('address')
+            <p class="error-message">{{$message}}</p>
+            @enderror
+
+            <input type="text" name="phone_number" placeholder="Phone Number"><br><br>
+            @error('phone_number')
+            <p class="error-message">{{$message}}</p>
+            @enderror
+
+            <label for="user_type">User Type:</label>
             <select name="user_type" id="user_type" required>
                 <option value="" disabled selected>Select User Type</option>
                 <option value="Customer">Customer</option>
                 <option value="Admin">Admin</option>
             </select><br><br>
             @error('user_type')
-                <p class="user-type-error">{{$message}}</p>
+            <p class="error-message">{{$message}}</p>
             @enderror
-            <input type="submit" value="Sign Up">
-            <a href="/login">Already registered</a>
+
+            <button type="submit">Sign Up</button>
+            <p>Already registered? <a href="/login">Log in</a></p>
         </form>
     </main>
     <footer>
@@ -116,7 +124,6 @@
         align-items: center;
     }
 
-    /* Navigation bar styles */
     nav {
         position: fixed;
         top: 0;
@@ -136,7 +143,6 @@
         color: var(--blue);
     }
 
-    /* Your signup form styles here */
     form {
         font-size: 1.69em;
         padding: 20px;
@@ -144,13 +150,65 @@
         border: 2px solid var(--darkgreen);
         border-radius: 10px;
         width: 500px;
+        text-align: center; 
+        margin: 20px auto; 
     }
 
+
     form input {
+        width: 470px;
+        height: 15px; 
+        padding: 8px;
+        margin: 4px;
+    }
+
+    h1 {
+        font-size: 1em;
+        color: var(--darkgreen);
+        margin-bottom: 10px;
+    }
+
+    .error-message {
+        color: red;
+        font-size: 1rem;
+        margin-top: 5px;
+    }
+23
+    label {
+        display: block;
+        margin-bottom: 5px;
+        color: var(--darkgreen);
+        font-size: 1rem;
+    }
+
+    select {
         width: 100%;
-        height: 30px;
         padding: 8px;
         margin: 10px 0;
+        box-sizing: border-box;
+    }
+
+    button {
+        background-color: var(--darkgreen);
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 1rem;
+        transition: background-color 0.2s ease-in-out;
+    }
+
+    button:hover {
+        background-color: var(--blue);
+    }
+
+    a {
+        color: var(--black);
+        text-decoration: none;
+        font-size: 1rem;
+        margin-top: 10px;
+        display: inline-block;
     }
 </style>
 
