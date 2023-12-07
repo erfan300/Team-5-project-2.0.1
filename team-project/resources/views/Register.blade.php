46113c9cb0f7ef7b85a7631ed3a22e92e4a5552a
MrGul1 <210059645@aston.ac.uk>
2023-12-07 18:56:59 +0000

    register page html and css
    updated the css and html for the register page to mka eit more user friendly etc also made it more functional with the navbar and moved the css to seperate area

--- team-project/resources/views/Register.blade.php
+++ team-project/resources/views/Register.blade.php
@@ -6,24 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Books4U Sign Up</title>
     <link rel="icon" href="" type="" />
+    <link href="{{ asset('css/register.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <script defer src="js/main.js"></script>
 </head>
 
 <body>
-    <header>
-    </header>
-    <main>
+    <header class="header">
+        <h1>Books4U BookStore</h1>
         <nav>
-            <a href="home">Home</a>
-            <a href="profile">Profile</a>
-            <a href="basket">Basket</a>
-            <a href="login">Log In</a>
-            <a href="about">About</a>
-            <a href="contact">Contact</a>
+            <a href="home"><i class="fas fa-home"></i> Home</a>
+            <a href="profile"><i class="fas fa-user"></i> Profile</a>
+            <a href="basket"><i class="fas fa-shopping-basket"></i> Basket</a>
+            <a href="login"><i class="fas fa-sign-in-alt"></i> Log In</a>
+            <a href="register"><i class="fas fa-user-plus"></i> Register</a>
+            <a href="about"><i class="fas fa-info-circle"></i> About</a>
+            <a href="contact"><i class="fas fa-envelope"></i> Contact</a>
         </nav>
+    </header>
 
+    <main>
         <form method="POST" action="/users">
-            <h1>Sign up</h1>
+            <h2>Sign up</h2>
             @if ($errors->any())
             <div class="alert">
                 <ul>
@@ -34,60 +38,81 @@
             </div>
             @endif
             @csrf
-            <input type="text" name="first_name" placeholder="First Name" required><br><br>
-            @error('first_name')
-            <p class="error-message">{{$message}}</p>
-            @enderror
 
-            <input type="text" name="last_name" placeholder="Last Name" required><br><br>
-            @error('last_name')
-            <p class="error-message">{{$message}}</p>
-            @enderror
+            <div class="input-box">
+                <input type="text" name="first_name" placeholder="First Name" required>
+                @error('first_name')
+                <p class="error-message">{{$message}}</p>
+                @enderror
+            </div>
 
-            <input type="text" name="username" placeholder="Username" required><br><br>
-            @error('username')
-            <p class="error-message">{{$message}}</p>
-            @enderror
+            <div class="input-box">
+                <input type="text" name="last_name" placeholder="Last Name" required>
+                @error('last_name')
+                <p class="error-message">{{$message}}</p>
+                @enderror
+            </div>
 
-            <input type="email" name="email" placeholder="Email Address" required><br><br>
-            @error('email')
-            <p class="error-message">{{$message}}</p>
-            @enderror
+            <div class="input-box">
+                <input type="text" name="username" placeholder="Username" required>
+                @error('username')
+                <p class="error-message">{{$message}}</p>
+                @enderror
+            </div>
 
-            <input type="email" name="email_confirmation" placeholder="Confirm Email Address" required><br><br>
-            @error('email_confirmation')
-            <p class="error-message">{{$message}}</p>
-            @enderror
+            <div class="input-box">
+                <input type="email" name="email" placeholder="Email Address" required>
+                @error('email')
+                <p class="error-message">{{$message}}</p>
+                @enderror
+            </div>
 
-            <input type="password" name="password" placeholder="Password" required><br><br>
-            @error('password')
-            <p class="error-message">{{$message}}</p>
-            @enderror
+            <div class="input-box">
+                <input type="email" name="email_confirmation" placeholder="Confirm Email" required>
+                @error('email_confirmation')
+                <p class="error-message">{{$message}}</p>
+                @enderror
+            </div>
 
-            <input type="password" name="password_confirmation" placeholder="Confirm Password" required><br><br>
-            @error('password_confirmation')
-            <p class="error-message">{{$message}}</p>
-            @enderror
+            <div class="input-box">
+                <input type="password" name="password" placeholder="Password" required>
+                @error('password')
+                <p class="error-message">{{$message}}</p>
+                @enderror
+            </div>
 
-            <input type="text" name="address" placeholder="Address"><br><br>
-            @error('address')
-            <p class="error-message">{{$message}}</p>
-            @enderror
+            <div class="input-box">
+                <input type="password" name="password_confirmation" placeholder="Confirm Password" required>
+                @error('password_confirmation')
+                <p class="error-message">{{$message}}</p>
+                @enderror
+            </div>
 
-            <input type="text" name="phone_number" placeholder="Phone Number"><br><br>
-            @error('phone_number')
-            <p class="error-message">{{$message}}</p>
-            @enderror
+            <div class="input-box">
+                <input type="text" name="address" placeholder="Address">
+                @error('address')
+                <p class="error-message">{{$message}}</p>
+                @enderror
+            </div>
 
-            <label for="user_type">User Type:</label>
-            <select name="user_type" id="user_type" required>
-                <option value="" disabled selected>Select User Type</option>
-                <option value="Customer">Customer</option>
-                <option value="Admin">Admin</option>
-            </select><br><br>
-            @error('user_type')
-            <p class="error-message">{{$message}}</p>
-            @enderror
+            <div class="input-box">
+                <input type="text" name="phone_number" placeholder="Phone Number">
+                @error('phone_number')
+                <p class="error-message">{{$message}}</p>
+                @enderror
+            </div>
+
+            <div class="input-box">
+                <label for="user_type">User Type:</label>
+                <select name="user_type" id="user_type" required>
+                    <option value="" disabled selected>Select User Type</option>
+                    <option value="Customer">Customer</option>
+                    <option value="Admin">Admin</option>
+                </select>
+                @error('user_type')
+                <p class="error-message">{{$message}}</p>
+                @enderror
+            </div>
 
             <button type="submit">Sign Up</button>
             <p>Already registered? <a href="/login">Log in</a></p>
@@ -96,120 +121,5 @@
     <footer>
     </footer>
 </body>
-
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap');
-
-    :root {
-        --blue: #6c96b6;
-        --darkgreen: #384b42;
-        --lightgreen: #6a9b86;
-        --border: 0.1rem solid #6a9b86;
-    }
-
-    body {
-        font-family: 'Roboto', sans-serif;
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        outline: none;
-        border: none;
-        text-decoration: none;
-        text-transform: capitalize;
-        transition: 0.2s linear;
-        background: var(--darkgreen);
-        min-height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    nav {
-        position: fixed;
-        top: 0;
-        right: 0;
-        padding: 20px;
-    }
-
-    nav a {
-        margin-left: 20px;
-        color: white;
-        text-decoration: none;
-        font-size: 1.6rem;
-        transition: color 0.2s linear;
-    }
-
-    nav a:hover {
-        color: var(--blue);
-    }
-
-    form {
-        font-size: 1.69em;
-        padding: 20px;
-        background-color: var(--lightgreen);
-        border: 2px solid var(--darkgreen);
-        border-radius: 10px;
-        width: 500px;
-        text-align: center; 
-        margin: 20px auto; 
-    }
-
-
-    form input {
-        width: 470px;
-        height: 15px; 
-        padding: 8px;
-        margin: 4px;
-    }
-
-    h1 {
-        font-size: 1em;
-        color: var(--darkgreen);
-        margin-bottom: 10px;
-    }
-
-    .error-message {
-        color: red;
-        font-size: 1rem;
-        margin-top: 5px;
-    }
-
-    label {
-        display: block;
-        margin-bottom: 5px;
-        color: var(--darkgreen);
-        font-size: 1rem;
-    }
-
-    select {
-        width: 100%;
-        padding: 8px;
-        margin: 10px 0;
-        box-sizing: border-box;
-    }
-
-    button {
-        background-color: var(--darkgreen);
-        color: white;
-        padding: 10px 20px;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        font-size: 1rem;
-        transition: background-color 0.2s ease-in-out;
-    }
-
-    button:hover {
-        background-color: var(--blue);
-    }
-
-    a {
-        color: var(--black);
-        text-decoration: none;
-        font-size: 1rem;
-        margin-top: 10px;
-        display: inline-block;
-    }
-</style>
 
 </html>
