44883b0ddbdec4d8735cc7b8aa7a347c77d05ed7
PRANAVgx <148206699+PRANAVgx@users.noreply.github.com>
2023-12-07 03:49:36 +0000

    Previous Orders Implementation
    Been working on this feature from sometime, it is currently premature . But needs some thorough testing

---
+++ team-project/app/Http/Controllers/PrevOrdersController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\InventoryLog;
+
+class PrevOrdersController extends Controller
+{
+    public function index()
+{
+    $userId = auth()->user()->User_ID;
+
+    $userOrders = InventoryLog::where('Customer_ID', $userId)->get();
+    
+    return view('profile', compact('userOrders'));
+}
+
+    
+    public function processReturn(Request $request)
+    {
+        // Handle the return process and update the stock level
+        $logId = $request->input('log_id');
+
+        $inventoryLog = InventoryLog::find($logId);
+
+        if ($inventoryLog) {
+            // Assuming the return process involves increasing the stock level
+            $newStockLevel = $inventoryLog->NewStockLevel + $inventoryLog->TransactionQuantity;
+            $inventoryLog->NewStockLevel = $newStockLevel;
+            $inventoryLog->save();
+        }
+
+        // Redirect back to the profile page (assuming it's named profile.blade.php)
+        return redirect()->route('prev-orders.index');
+    }
+}
