f3722535aba694da972cd079671458dcbb1d6c18
MrGul1 <210059645@aston.ac.uk>
2023-12-10 14:53:23 +0000

    navbar update
    added navbar to basket search show single page

--- team-project/resources/views/single.blade.php
+++ team-project/resources/views/single.blade.php
@@ -7,6 +7,7 @@
     <link rel="icon" href="" type="" />
     <link rel="stylesheet" type="text/css" href="css/single.css" />
     <link href="{{ asset('css/single.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <script defer src="js/main.js"></script>
 </head>
 
@@ -17,12 +18,13 @@
 
     <!-- Navigation -->
     <nav>
-        <a href="home">Home</a>
-        <a href="profile">Profile</a>
-        <a href="basket">Basket</a>
-        <a href="login">Log In</a>
-        <a href="about">About</a>
-        <a href="contact">Contact</a>
+        <a href="home"><i class="fas fa-home"></i> Home</a>
+        <a href="profile"><i class="fas fa-user"></i> Profile</a>
+        <a href="basket"><i class="fas fa-shopping-basket"></i> Basket</a>
+        <a href="login"><i class="fas fa-sign-in-alt"></i> Log In</a>
+        <a href="register"><i class="fas fa-user-plus"></i> Register</a>
+        <a href="about"><i class="fas fa-info-circle"></i> About</a>
+        <a href="contact"><i class="fas fa-envelope"></i> Contact</a>
     </nav>
     <br><br>
 
