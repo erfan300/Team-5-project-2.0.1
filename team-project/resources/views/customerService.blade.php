7b0441fe568e0990219e94b4bf06fc2403859084
TamaraFufeyin <220068149@aston.ac.uk>
2024-02-06 20:25:02 +0000

    created a customer service page

---
+++ team-project/resources/views/customerService.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Books4U</title>
+    <link rel="icon" href="" type="" />
+    <link rel="stylesheet" type="text/css" href="css/basket.css" />
+    <script defer src="js/main.js"></script>
+</head>
+
+    <title>Books4U Bookstore</title>
+
+    <body>
+    <div class="container">
+    <h1>Contact Customer Service</h1>
+    //potentially goes into table<form action="" method="post">//
+        <label for="name">Your Name:</label>
+        <input type="text" id="name" name="name" required>
+
+        <label for="email">Your Email:</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="subject">Subject:</label>
+        <input type="text" id="subject" name="subject" required>
+
+        <label for="message">Message:</label>
+        <textarea id="message" name="message" required></textarea>
+
+        <input type="submit" value="Submit">
+    </form>
+</div>
+
+
+
+
+
+
+
+
+
+
+    </body>
+
+</html>
