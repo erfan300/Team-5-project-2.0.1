edf395d811d046a33057e7360d942de1ddde18eb
TamaraFufeyin <220068149@aston.ac.uk>
2024-02-21 11:33:50 +0000

    Fixing page layout

--- team-project/resources/views/Basket.blade.php
+++ team-project/resources/views/Basket.blade.php
@@ -12,33 +12,47 @@
 </head>
 
 <body>
-    <!-- Display login flash Message -->
-    @if (session('message'))
+   
+<!-- Display login flash Message -->
+@if (session('message'))
         <div class="alert alert-success">
             {{ session('message') }}
         </div>
     @endif
-    <header>
+
+
     <header>
         <h1>Books4U Bookstore</h1>
+        @auth
+            <div class="log-out-box">
+                <span>Welcome {{ auth()->user()->Username }}</span>
+                <form class="inLine" method="POST" action="/logout">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+            </div>
+        @endauth
     </header>
 
-    <!-- Navigation -->
-   
-
-<nav>
-    <a href="home"><i class="fas fa-home"></i> Home</a>
-    <a href="profile"><i class="fas fa-user"></i> Profile</a>
-    <a href="basket"><i class="fas fa-shopping-basket"></i> Basket</a>
-    <a href="wishlist"><i class="fas fa-heart"></i> Wishlist</a>
-    <a href="login"><i class="fas fa-sign-in-alt"></i> Log In</a>
-    <a href="register"><i class="fas fa-user-plus"></i> Register</a>
-    <a href="about"><i class="fas fa-info-circle"></i> About</a>
-    <a href="contact"><i class="fas fa-envelope"></i> Contact</a>
+    <nav>
+        <a href="home"><i class="fas fa-home"></i> Home</a>
+        <a href="profile"><i class="fas fa-user"></i> Profile</a>
+        <a href="basket"><i class="fas fa-shopping-basket"></i> Basket</a>
+        <a href="wishlist"><i class="fas fa-heart"></i> Wishlist</a>
+        
+        @if(Auth::check() && Auth::user()->User_Type === 'Admin')
+            <a href="create"><i class="fas fa-plus"></i> Create</a>
+            <a href="search"><i class="fas fa-search"></i> Search</a>
+            <a href="list"><i class="fas fa-list"></i> List</a>
+            <a href="{{ route('order-report') }}"><i class="far fa-file-alt"></i> Order Reports</a>
+        @endif
+        
+        <a href="about"><i class="fas fa-info-circle"></i> About</a>
+        <a href="contact"><i class="fas fa-envelope"></i> Contact</a>
     </nav>
+    
 
 
-    <br><br>
 
     <!-- Shopping Basket -->
 
