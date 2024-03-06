8b68ea93dc11aebc60a4ca8f24730caf8b76b4da
dinagenn <220161026@aston.ac.uk>
2024-03-06 10:51:58 +0000

    Update profile.blade.php

--- team-project/resources/views/profile.blade.php
+++ team-project/resources/views/profile.blade.php
@@ -9,25 +9,8 @@
     <title>Profile</title>
 </head>
 
-<div class="top-left">
-        <div class="login-buttons">
-            <a href="login"><i class="fas fa-sign-in-alt"></i> Log In</a>
-            <a href="register"><i class="fas fa-user-plus"></i> Register</a>
-            @auth
-                <a href="profile"><i class="fas fa-user"></i> Profile</a>
-            @endauth
-        </div>
-    </div>
-    <h1>BOOKS<span>4</span>U</h1>
-    <div class="session-message">
-        @if (session('message'))
-            <div class="alert alert-success">
-                {{ session('message') }}
-            </div>
-        @endif
-    </div>
-    @auth
-      
+<body> 
+<h1>BOOKS<span>4</span>U</h1>
 
     <nav>
         <a href="home"><i class="fas fa-home"></i> Home</a>
