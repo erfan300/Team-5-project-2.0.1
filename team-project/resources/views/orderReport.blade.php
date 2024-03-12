a71afd40ac2000902ca320670746ad6c608b7331
MrGul1 <210059645@aston.ac.uk>
2024-03-12 16:59:26 +0000

    fixed the order report
    same thing done to product report was done to order report

--- team-project/resources/views/orderReport.blade.php
+++ team-project/resources/views/orderReport.blade.php
@@ -5,39 +5,56 @@
     <link href="{{ asset('css/orderReport.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
 </head>
-<body>
-    <h1>Order Reports</h1>
-    <div class="log-out-box">
-        @auth
-        <div>
+<header>
+    
+    <div class="top-left">
+        <div class="login-buttons">
+            <a href="login"><i class="fas fa-sign-in-alt"></i> Log In</a>
+            <a href="register"><i class="fas fa-user-plus"></i> Register</a>
+            @auth
+                <a href="profile"><i class="fas fa-user"></i> Profile</a>
+            @endauth
+        </div>
+    </div>
+    <h1>BOOKS<span>4</span>U</h1>
+    <div class="session-message">
+        @if (session('message'))
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
+        @endif
+    </div>
+    @auth
+        <div class="log-out-box">
+            <form class="inLine" method="POST" action="/logout">
+                @csrf
+                <button type="submit"><i class="fas fa-sign-out-alt"></i> Logout</button>
+            </form>
+        </div>
+        <div class="welcome-message">
             <span>Welcome {{ auth()->user()->Username }}</span>
         </div>
-        <form class="inLine" method="POST" action="/logout">
-            @csrf
-            <button type="submit">Logout</button>
-        </form>
-        @endauth
-    </div>
+    @endauth
+</header>
+
+<nav>
+    <a href="home"><i class="fas fa-home"></i> Home</a>
+    <a href="basket"><i class="fas fa-shopping-basket"></i> Basket</a>
+    <a href="wishlist"><i class="fas fa-heart"></i> Wishlist</a>
     
-    <nav>
-        <a href="home"><i class="fas fa-home"></i> Home</a>
-        <a href="profile"><i class="fas fa-user"></i> Profile</a>
-        <a href="basket"><i class="fas fa-shopping-basket"></i> Basket</a>
-        <a href="wishlist"><i class="fas fa-heart"></i> Wishlist</a>
-        
-        @if(Auth::check() && Auth::user()->User_Type === 'Admin')
-            <a href="create"><i class="fas fa-plus"></i> Create</a>
-            <a href="search"><i class="fas fa-search"></i> Search</a>
-            <a href="list"><i class="fas fa-list"></i> List</a>
-            <a href="{{ route('order-report') }}"><i class="far fa-file-alt"></i> Order Reports</a>
-            <a href="{{ route('product-report') }}"><i class="far fa-file-alt"></i> Product Report</a>
-        @endif
-        
-        <a href="login"><i class="fas fa-sign-in-alt"></i> Log In</a>
-        <a href="register"><i class="fas fa-user-plus"></i> Register</a>
-        <a href="about"><i class="fas fa-info-circle"></i> About</a>
-        <a href="contact"><i class="fas fa-envelope"></i> Contact</a>
-    </nav>
+    @if(Auth::check() && Auth::user()->User_Type === 'Admin')
+        <a href="create"><i class="fas fa-plus-circle"></i> Create</a>
+        <a href="search"><i class="fas fa-search"></i> Search</a>
+        <a href="list"><i class="fas fa-list"></i> List</a>
+        <a href="{{ route('order-report') }}"><i class="fas fa-chart-bar"></i> Order Report</a>
+        <a href="{{ route('product-report') }}"><i class="fas fa-chart-pie"></i> Product Report</a>
+        <a href="{{ route('discountpage') }}"><i class="fas fa-tags"></i> Discount Page</a>
+    @endif
+    
+    <a href="about"><i class="fas fa-info-circle"></i> About</a>
+    <a href="contact"><i class="fas fa-envelope"></i> Contact</a>
+</nav>
+
     <table>
         <thead>
             <tr>
