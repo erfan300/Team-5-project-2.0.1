5c6ace3593d29fa1abb2e2c954605734e42b8250
MrGul1 <210059645@aston.ac.uk>
2024-03-12 17:06:47 +0000

    fixes search bar routing and html css
    fixed the routing was unable to open search bar before updated the html to look cleaner and match the index, updated the footer and header and searchbar

--- team-project/resources/views/search.blade.php
+++ team-project/resources/views/search.blade.php
@@ -7,19 +7,11 @@
     <link href="{{ asset('css/homeStyle.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/search.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
-    <title>Search Books | Book4U</title>
-</head>
-<body>
-<!-- Display login flash Message -->
-@if (session('message'))
-        <div class="alert alert-success">
-            {{ session('message') }}
-        </div>
-    @endif
-
-
+    
+    
     <header>
-        <div class="top-left">
+    
+    <div class="top-left">
         <div class="login-buttons">
             <a href="login"><i class="fas fa-sign-in-alt"></i> Log In</a>
             <a href="register"><i class="fas fa-user-plus"></i> Register</a>
@@ -48,37 +40,35 @@
         </div>
     @endauth
 </header>
-    </header>
 
-    <!-- Navigation -->
-    <nav>
-        <a href="{{ route('home') }}"><i class="fas fa-home"></i> Home</a>
-        <a href="{{ route('profile') }}"><i class="fas fa-user"></i> Profile</a>
-        <a href="{{ route('basket') }}"><i class="fas fa-shopping-basket"></i> Basket</a>
-        <a href="{{ route('wishlist') }}"><i class="fas fa-heart"></i> Wishlist</a>
-        
-        @if(Auth::check() && Auth::user()->User_Type === 'Admin')
-            <a href="{{ route('create') }}"><i class="fas fa-plus"></i> Create</a>
-            <a href="{{ route('search') }}"><i class="fas fa-search"></i> Search</a>
-            <a href="{{ route('list') }}"><i class="fas fa-list"></i> List</a>
-            <a href="{{ route('order-report') }}"><i class="far fa-file-alt"></i> Order Reports</a>
-            <a href="{{ route('product-report') }}"><i class="far fa-file-alt"></i> Product Report</a>
-        @endif
-        
-        <a href="{{ route('about') }}"><i class="fas fa-info-circle"></i> About</a>
-        <a href="{{ route('contact') }}"><i class="fas fa-envelope"></i> Contact</a>
-    </nav>
-    <br><br>
+<nav>
+    <a href="home"><i class="fas fa-home"></i> Home</a>
+    <a href="basket"><i class="fas fa-shopping-basket"></i> Basket</a>
+    <a href="wishlist"><i class="fas fa-heart"></i> Wishlist</a>
+    
+    @if(Auth::check() && Auth::user()->User_Type === 'Admin')
+        <a href="create"><i class="fas fa-plus-circle"></i> Create</a>
+        <a href="search"><i class="fas fa-search"></i> Search</a>
+        <a href="list"><i class="fas fa-list"></i> List</a>
+        <a href="{{ route('order-report') }}"><i class="fas fa-chart-bar"></i> Order Report</a>
+        <a href="{{ route('product-report') }}"><i class="fas fa-chart-pie"></i> Product Report</a>
+        <a href="{{ route('discountpage') }}"><i class="fas fa-tags"></i> Discount Page</a>
+    @endif
+    
+    <a href="about"><i class="fas fa-info-circle"></i> About</a>
+    <a href="contact"><i class="fas fa-envelope"></i> Contact</a>
+</nav>
 
-     <!-- Search Bar -->
-     <form action="/search" class="search-form">
+
+  <!-- Search Bar -->
+  <form action="/search" class="search-form">
         <div class="search-container">
             <input type="text" name="search" class="search-input" placeholder="Search Books4U..." />
             <div class="search-button-container">
                 <button type="submit" class="search-button">Search</button>
             </div>
         </div>
-        <div class="filter-link">Filter
+        <div class="filter-link"><i class="fas fa-sort-amount-down"></i>
             <div class="filter-dropdown">
                 <div class="filter-categories">
                     <div class="filter-category">
@@ -118,7 +108,7 @@
                 </div>
             </div>
         </div>
-    </form> 
+    </form>
     <div class="search-book-container">
         @if(count($books) == 0)
             <p> No Books found </p>
@@ -130,6 +120,55 @@
     <div class="pagination-container"> 
         {{$books->appends(request()->query())->links()}} <!-- This is the button to show next page of books -->
     </div>
+
+
+    <!-- FOOTER -->
+    <section class="footer">
+        <div class="footer-content">
+            <div class="footer-section about">
+                <h3>About Us</h3>
+                <p>Welcome to Books4U, the place where the love for writing intertwines with the art of storytelling. We strongly believe in the transformative power of books, and their ability to greatly impact individuals, as well as communities. Our main purpose is to inspire, attract, and ignite the imaginations of the people who enjoy reading books.</p>
+                <section class="footer-section contact">
+    <h3>Contact Us</h3>
+    <ul>
+        <li><i class="fas fa-phone"></i> Phone: +44 0121 456 7894</li>
+        <li><i class="fas fa-envelope"></i> Email: Books4U@gmail.com</li>
+        <div class="social-media">
+        <a href="https://www.instagram.com/your_instagram" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.facebook.com/your_facebook" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="https://twitter.com/your_twitter" target="_blank"><i class="fab fa-twitter"></i></a>
+        </div>
+    </ul>
+</section>
+            </div>
+            <div class="footer-section links">
+                <h3>Quick Links</h3>
+                <a href="about"><i class="fas fa-info-circle"></i> About</a>
+                <a href="contact"><i class="fas fa-envelope"></i> Contact</a>
+                <a href="home"><i class="fas fa-home"></i> Home</a>
+                <a href="login"><i class="fas fa-sign-in-alt"></i> Log In</a>
+                <a href="register"><i class="fas fa-user-plus"></i> Register</a>
+            </div>
+            <div class="footer-section contact-form">
+                <h3>Contact Us</h3>
+                <form action="#" method="post">
+                    <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
+                    <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+                    <button type="submit" class="btn btn-big contact-btn">
+                        <i class="fas fa-envelope"></i>
+                        Send
+                    </button>
+                </form>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            &copy; 2024 Books4U Bookstore. All rights reserved.
+        </div>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.9/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
 
