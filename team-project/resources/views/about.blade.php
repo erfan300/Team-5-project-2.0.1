f8d0dd94b083351809420a10381e5f9559aa8a48
dinagenn <220161026@aston.ac.uk>
2024-03-13 09:33:41 +0000

    Update about.blade.php

--- team-project/resources/views/about.blade.php
+++ team-project/resources/views/about.blade.php
@@ -11,34 +11,9 @@
     <link href="{{ asset('css/aboutcss.css') }}" rel="stylesheet">
 </head>
 
-<body>
-<header>
-    
-    <div class="top-left">
-        <div class="login-buttons">
-            <a href="login"><i class="fas fa-sign-in-alt"></i> Log In</a>
-            <a href="register"><i class="fas fa-user-plus"></i> Register</a>
-            @auth
-                <a href="profile"><i class="fas fa-user"></i> Profile</a>
-            @endauth
-        </div>
-    </div>
-    <h1>BOOKS<span>4</span>U</h1>
-    <div class="session-message">
-        @if (session('message'))
-            <div class="alert alert-success">
-                {{ session('message') }}
-            </div>
-        @endif
-    </div>
-    @auth
-        <div class="log-out-box">
-            <form class="inLine" method="POST" action="/logout">
-                @csrf
-                <button type="submit"><i class="fas fa-sign-out-alt"></i> Logout</button>
-            </form>
-        </div>
 
+    <h1>BOOKS<span>4</span>U</h1>
+    
 
 <body>
     
