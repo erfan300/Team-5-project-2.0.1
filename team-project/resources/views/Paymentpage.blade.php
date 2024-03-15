ab98f262d2d1addc2c74d23f031cd52376bdcb27
dinagenn <220161026@aston.ac.uk>
2024-03-15 16:55:48 +0000

    Update Paymentpage.blade.php

--- team-project/resources/views/Paymentpage.blade.php
+++ team-project/resources/views/Paymentpage.blade.php
@@ -61,6 +61,7 @@
 
 <section id="Payment page" class="container mt-5">
     <header>
+    
     <h2 class="title">Checkout</h2>
     <br>
     <header>
@@ -68,19 +69,19 @@
       <section>  
             <h3>Billing Address</h3>
             <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-           
+           <br>
             <input type="text" id="fname" name="firstname" placeholder="John Smith">
            <br>
             <label for="email"><i class="fa fa-envelope"></i> Email</label>
+            <br>
             <input type="text" id="email" name="email" placeholder="Johnsmith@example.com">
             <br>
-            <br>
-
-            
             <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+            <br>
             <input type="text" id="adr" name="address" placeholder="10 Wall Street">
             <br>>
                 <label for="zip" ></label > Postcode</label>
+                <br>
                 <input type="text" id="postcode" name="Postcode">
               </div>
             </div>
@@ -88,26 +89,39 @@
 <br>
 <br>
          
-<h2 class="title">Payment</h2>
+<h3 class="title">Payment</h3>
             <br>
+<label for="shipping_method">Shipping Method:</label><br>
+        <select id="shipping_method" name="shipping_method" required>
+            <option value="">Select Shipping Method</option>
+            <option value="standard">Standard Shipping</option>
+            <option value="express">Express Shipping</option>
+            <option value="next_day">Next Day Delivery</option>
+        </select><br><br>
+
+
             <br>
             <label for="cname">Name on Card</label>
+            <br>
             <input type="text" id="cname" name="cardname" placeholder="John Smith">
             <br>
             <label for="ccnum">Card number</label>
+            <br>
             <input type="text" id="account number" name="cardnumber" placeholder="1111-2222-3333-4444">
             <br>
             <label for="expmonth">Exp Month</label>
+            <br>
             <input type="text" id="ex-pmonth" name="exp-month" placeholder="September">
 <br>
-            <div class="row">
              
                 <label for="exp-year">Exp Year</label>
+                <br>
                 <input type="text" id="exp-year" name="exp-year" placeholder="2018">
                 <br>
               </div>
               
                 <label for="cvv">CVV</label>
+                <br>
                 <input type="text" id="cvv" name="cvv" placeholder="352">
               </div>
             </div>
@@ -120,17 +134,16 @@
        
         </label>
 
-        <label for="shipping_method">Shipping Method:</label><br>
-        <select id="shipping_method" name="shipping_method" required>
-            <option value="">Select Shipping Method</option>
-            <option value="standard">Standard Shipping</option>
-            <option value="express">Express Shipping</option>
-            <option value="next_day">Next Day Delivery</option>
-        </select><br><br>
+        
 
-        <input type="submit" value="Checkout">
-    </form>
-        <input type="submit" value="Purchase" class="btn">
+       
+    
+    <div class="purchase">
+            <a href="{{ route('payment-page') }}" class="btn btn-success">Purchase</a>
+        </div>
+
+
+        
       </form>
     </div>
   </div>
