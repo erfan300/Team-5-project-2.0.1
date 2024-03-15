22816a6104163c0da6da0bcaf249d77834bff3c9
dinagenn <220161026@aston.ac.uk>
2024-03-15 18:41:31 +0000

    css for payment page

--- team-project/resources/views/Paymentpage.blade.php
+++ team-project/resources/views/Paymentpage.blade.php
@@ -7,6 +7,8 @@
     <title>Books4U</title>
     <link rel="icon" href="" type="" />
     <link rel="stylesheet" type="text/css" href="css/payment.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+
     <script defer src="js/main.js"></script>
 </head>
 
