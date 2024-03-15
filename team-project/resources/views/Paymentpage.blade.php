597c453032b85cca88cc69872344eebbe8b1b2bc
dinagenn <220161026@aston.ac.uk>
2024-03-15 18:39:59 +0000

    css

--- team-project/resources/views/Paymentpage.blade.php
+++ team-project/resources/views/Paymentpage.blade.php
@@ -157,7 +157,7 @@
     </ul>
 </section>
             </div>
-        <div class="footer-section links">
+            <div class="footer-section links">
                 <h3>Quick Links</h3>
                 <a href="about"><i class="fas fa-info-circle"></i> About</a>
                 <a href="contact"><i class="fas fa-envelope"></i> Contact</a>
@@ -182,6 +182,9 @@
         </div>
     </section>
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.9/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
                        
